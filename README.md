22921475a1e94555320e8b8c1b334bde01de0702
wanghao <muyao1987@qq.com>
2021-11-16 13:40:36 +0800

    升级V3.0

--- README.md
+++ README.md
@@ -1,27 +1,49 @@
-# Mars2D最简项目模版 - ES5传统版
- 传统模式Web前端技术栈下的最简[Mars2D](http://mars2d.cn)应用的项目模版
+ <p align="center">
+<img src="http://mars2d.cn/logo.png" width="300px" />
+</p>
 
- > 其他技术栈，请参考： [https://github.com/marsgis/mars2d](https://github.com/marsgis/mars2d)
- 
+<p align="center">基于传统JS技术栈的 Mars2D🌎最简项目模板</p>
+
+<p align="center">
+<a target="_black" href="https://github.com/marsgis/mars2d">
+<img alt="GitHub stars" src="https://img.shields.io/github/stars/marsgis/mars2d?style=flat&logo=github">
+</a>
+<a target="_black" href="https://www.npmjs.com/package/mars2d">
+<img alt="Npm downloads" src="https://img.shields.io/npm/dt/mars2d?style=flat&logo=npm">
+</a>
+<a target="_black" href="https://www.npmjs.com/package/mars2d">
+<img alt="Npm version" src="https://img.shields.io/npm/v/mars2d.svg?style=flat&logo=npm&label=version"/>
+</a>
+</p>
  
 
-## 使用说明
+
+## 运行站点
  在任意开发编辑器（如vscode等）或http服务器(如node、nginx、tomcat、IIS等)下直接运行浏览index.html即可
 
-### 压缩及混淆
- build整站压缩及混淆：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+### 运行效果 
+ [在线Demo](http://mars2d.cn/project/es5-template/)  
 
-
-## 运行效果
- [在线Demo1](http://mars2d.cn/project/simple-es5/index.html) 、
- [在线Demo2](http://mars2d.cn/project/simple-es5/index.html?config=config/gcj.json)  、
- [在线Demo3](http://mars2d.cn/project/simple-es5/index.html?config=config/baidu.json) 
-
- ![image](http://mars2d.cn/project/img/simple-es5.jpg)
+ ![image](http://mars2d.cn/project/es5-template/screenshot.jpg)
  
- [更多项目体验](http://mars2d.cn/project.html)
+
+
+## 压缩及混淆
+ 如果需要编译、对整站压缩及混淆，请参考：[https://github.com/muyao1987/web-dist](https://github.com/muyao1987/web-dist)
+
+ 
+
+
+## Mars2D 是什么 
+  `Mars2D平台` 是[火星科技](http://marsgis.cn/)研发的一款免费的二维地图客户端开发平台，基于[Leaflet](http://leafletjs.com/)优化提升与B/S架构设计，支持多行业扩展的轻量级高效能GIS开发平台，提供了全新的大数据可视化、实时流数据可视化功能，通过本平台可快速实现浏览器和移动端上美观、流畅的地图呈现与空间分析，完成平台在不同行业的灵活应用。
+
+
+### 相关网站 
+- Mars2D官网：[http://mars2d.cn](http://mars2d.cn)  
+
+- Mars2D开源项目列表：[https://github.com/marsgis/mars2d](https://github.com/marsgis/mars2d)
 
 
 ## 版权说明
-1. 任何`个人或组织`可以在遵守Mars2D相关要求下`免费无限制`使用。
-2. 如有`个性化需求`或`商业应用`，请联系[火星科技](http://mars2d.cn)购买。
+1. Mars2D平台由[火星科技](http://marsgis.cn/)自主研发，拥有所有权利。
+2. 任何个人或组织可以在遵守相关要求下可以免费无限制使用。
--- js/index.js
+++ js/index.js
@@ -1,73 +1,25 @@
-//系统 主入口
+"use script"; //开发环境建议开启严格模式
 
-var map;    //地图对象 
-var request; //url传入的参数
+var map;
 
-$(document).ready(function () { 
-    //记录url传入参数
-    request = haoutil.system.getRequest();
-    if (window.top) {//有父级
-        request = haoutil.system.getRequest(window.top);
-    }
+//读取 config.json 配置文件
+let configUrl = "config/config.json";
+mars2d.axios
+  .get(configUrl)
+  .then(function (response) {
+    //构建地图
+    window.initMap(response.data.map);
+  })
+  .catch(function (error) {
+    console.log(error);
+    haoutil.alert(error && error.message, "出错了");
+  });
 
-    initMap();
-});
-
-function initMap() {
-    var configfile = "config/config.json"; //默认地址
-    if (request.config)//url传入地址
-        configfile = request.config;
-
-    haoutil.loading.show();
-    $.ajax({
-        type: "get",
-        dataType: "json",
-        url: configfile,
-        timeout: 0,
-        success: function (data) {
-            haoutil.loading.hide(); 
- 
-            //构造地图
-            map = L.mars.createMap({
-                id: "map",
-                data: data.map,
-                serverURL: data.serverURL, 
-                layerToMap:layerToMap
-            });
- 
-            //如果有xyz传参，进行定位 
-            if (haoutil.isutil.isNotNull(request.x)
-                && haoutil.isutil.isNotNull(request.y)
-                && haoutil.isutil.isNotNull(request.z)) {
-                var x = Number(request.x);
-                var y = Number(request.y);
-                var z = Number(request.z);
-                map.setView([y, x], z);
-            }
-
-            initWork(map);  //项目的其他事项 
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            haoutil.loading.hide();
-            haoutil.alert(configfile + "文件加载失败！");
-        }
-    });
   
+function initMap(options) {
+  //合并属性参数，可覆盖config.json中的对应配置
+  let mapOptions = mars2d.Util.merge(options, {});
+
+  //创建地图
+  map = new mars2d.Map("mars2dContainer", mapOptions);
 }
- 
-
-//当前页面业务相关
-function initWork() {
-
-
-}
-
-
-//自定义图层添加方法
-function layerToMap(config, layer) {
-    if (config.type == "wfs") {
-        layer = L.wfsLayer(config);//wfs插件
-        return layer;
-    }
-};
- 
