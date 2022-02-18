cb58aba126eacc1c87acabb1e59d4727638cd6bb
wanghao <muyao1987@qq.com>
2022-02-18 17:48:38 +0800

    v3.0.6

--- js/index.js
+++ js/index.js
@@ -3,19 +3,15 @@
 var map;
 
 //读取 config.json 配置文件
-let configUrl = "config/config.json";
-mars2d.axios
-  .get(configUrl)
-  .then(function (response) {
-    //构建地图
-    window.initMap(response.data.map);
+mars2d.Util.fetchJson({ url: "config/config.json" })
+  .then(function (data) {
+    window.initMap(data.mars2d); //构建地图
   })
   .catch(function (error) {
     console.log(error);
     haoutil.alert(error && error.message, "出错了");
   });
 
-  
 function initMap(options) {
   //合并属性参数，可覆盖config.json中的对应配置
   let mapOptions = mars2d.Util.merge(options, {});
